47d0c9525e244a3f57f3c73c34f17b6eb1fb1adf
MorenaFischione <fischione.morena@gmail.com>
2021-10-29 16:33:48 +0200

    importazione di fontawesome e creazione di area price su comic.blade.php

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -6,14 +6,25 @@
     <div id="card-detail">
         <div class="description">
             <h1>{{ $comic['title'] }}</h1>
-            <p>US Price {{ $comic['price'] }}</p>
+            <div class="wrapper-buy">
+                <div class="price">
+                    <p>US Price {{ $comic['price'] }}</p>
+                    <p>AVAILABLE</p>
+                </div>
+                <div class="check">
+                    <p>Check Available <i class="fas fa-angle-down"></i></p>
+                </div>
+            </div>
             <p>{{ $comic['description'] }}</p>
         </div>
         <div class="img">
-            <img src="{{asset('images/adv.jpg')}}" alt="">
-            
+            <img src="{{asset('images/adv.jpg')}}" alt="immagine di superman che vola">
         </div>
         
         
     </div>
+@endsection
+
+@section('cdn-import')
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css">
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Comics | @yield('title')</title>
     <link rel="stylesheet" href=" {{ asset('css/app.css') }}">
+    @yield('cdn-import')
 </head>
 <body>
     @include('partials.header', ['links' => config('header_main_menu')])
